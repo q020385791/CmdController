9615496045c3183d5e543ac288a76a39d8ddbd71
sai wanza <q020385791@gmail.com>
2021-05-31 10:58:34 +0800

    加入專案檔案。

---
+++ CmdController/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CmdController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process p = new Process();
+            String str = null;
+
+            p.StartInfo.FileName = "cmd.exe";
+
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            //p.StartInfo.CreateNoWindow = true; //不跳出cmd視窗
+
+            p.Start();
+            p.StandardInput.WriteLine("ipconfig");
+            p.StandardInput.WriteLine("exit");
+
+            str = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            p.Close();
+
+
+            Console.WriteLine(str);
+            Console.ReadKey();
+
+            //來源 https://dotblogs.com.tw/jj_lifenote/2018/04/24/200342
+        }
+    }
+}
